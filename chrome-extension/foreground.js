0255d0472e5bffad80d4ddf83785f8e16fbc549b
Tianning Li <51225681+lit26@users.noreply.github.com>
2022-01-11 23:09:50 -0800

    Update foreground.js

--- chrome-extension/foreground.js
+++ chrome-extension/foreground.js
@@ -1,14 +1,8 @@
-// let pageContent = document.getElementsByTagName("body")[0];
-
-// const requestOptions = {
-//   method: "POST",
-//   headers: { "Content-Type": "application/json" },
-//   body: JSON.stringify({ page: "test" }),
-// };
 fetch("http://127.0.0.1:5005/api/page", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
+    url: window.location.href,
     page: document.getElementsByTagName("body")[0].innerHTML,
   }),
 }).catch((err) => console.log(error));
